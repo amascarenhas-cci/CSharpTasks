a036b4abe2826139d23cd04fb745befd91300edc
Ashwin Mascarenhas <ashwin.mascarenhas@creativecapsule.com>
2022-09-23 11:49:36 +0530

    Task1: Calculate age in Days

---
+++ Task1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpTasks
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter your age in years");
+            age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Age in Days: "+ calcAge(age));
+        }
+
+        static int calcAge(int age)
+        {
+            age=age*365;
+            return age;
+        }
+
+    }
+    
+}
+
