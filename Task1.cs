6f077738f44ebd7ee537b3d941645e21fb970168
Ashwin Mascarenhas <ashwin.mascarenhas@creativecapsule.com>
2022-09-26 10:45:24 +0530

    Task 4 added

--- Task1.cs
+++ Task1.cs
@@ -6,14 +6,14 @@ namespace CSharpTasks
     {
         static void Main(string[] args)
         {
-            //Task2 t2=new Task2();
-            //Task3 t3=new Task3();
+            Task2 t2=new Task2();
+            Task3 t3=new Task3();
             Task4 t4=new Task4();
 
-            //int age,number;
-            string[] properOrder= new string[6];
+            int age,number;
+            string properOrder;
 
-            /*Console.WriteLine("Enter your age in years");
+            Console.WriteLine("Enter your age in years");
             age=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Age in Days: "+ calcAge(age));
 
@@ -23,16 +23,11 @@ namespace CSharpTasks
 
             Console.WriteLine("Enter a number ");
             number=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Result:"+ t3.cubes(number));*/
+            Console.WriteLine("Result:"+ t3.cubes(number));
 
             Console.WriteLine("Enter a string to be reversed");
-            for(int i=0;i<6;i++)
-            {
-              properOrder[i]=Console.ReadLine();
-            }
-
-             t4.ReversedString(properOrder);
-            //Console.WriteLine("Reversed String"+ t4.ReversedString(properOrder));
+            properOrder=Console.ReadLine();
+            Console.WriteLine("Reversed String: "+ t4.ReversedString(properOrder));
         }
 
         static int calcAge(int age)
